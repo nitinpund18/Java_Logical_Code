1922657d00c21636428e661c02a63787c524012a
Shree <Shree@DESKTOP-5GK796K>
2023-03-28 03:18:51 -0700

    6th

---
+++ src/Java_Logical_Programs/Convert_String_to_Char_Java.java
@@ -0,0 +1,12 @@
+package Java_Logical_Programs;
+
+public class Convert_String_to_Char_Java 
+{
+public static void main(String[] args) 
+{
+	String str="nitin pund";
+	char c=str.charAt(0);
+	System.out.println("1st character is " + c);
+
+}
+}
