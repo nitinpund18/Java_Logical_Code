b11d47e23970d88a0a28fb9f2b3303a9b7a3f2e0
Shree <Shree@DESKTOP-5GK796K>
2023-03-28 03:41:37 -0700

    11

---
+++ src/Java_Logical_Programs/Given_No_Odd_Even.java
@@ -0,0 +1,17 @@
+package Java_Logical_Programs;
+
+public class Given_No_Odd_Even 
+{
+public static void main(String[] args)
+{
+	int num=13;
+	if(num%2==0)
+	{
+		System.out.println("Entred Number is Even");
+	}
+	else
+	{
+		System.out.println("Entred Number is Odd");
+	}
+}
+}
