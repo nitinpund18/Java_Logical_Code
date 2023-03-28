cf32078bd2d37eeb0939bc7e1feddf81f77bbbd6
Shree <Shree@DESKTOP-5GK796K>
2023-03-28 03:27:51 -0700

    8th

--- src/Java_Logical_Programs/Duplicate_Element.java
+++ src/Java_Logical_Programs/Duplicate_Element.java
@@ -1,5 +1,18 @@
 package Java_Logical_Programs;
 
-public class Duplicate_Element {
-
+public class Duplicate_Element 
+{
+public static void main(String[] args) 
+{
+	int a[] = {1,2,3,4,2,7,8,8,3};
+	System.out.println("Duplicate element in given array");
+	for(int i=0;i<a.length;i++)
+	{
+		for(int j=i+1 ;i<a.length;j++)
+		{
+			if(a[i]==a[j])
+				System.out.println(a[i]);
+		}
+	}
+}
 }
