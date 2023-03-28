5995dbe5eebf7ba4e89780d590d268c58fc2b35d
Shree <Shree@DESKTOP-5GK796K>
2023-03-28 03:44:34 -0700

    12

---
+++ src/Java_Logical_Programs/Odd_Even_No.java
@@ -0,0 +1,25 @@
+package Java_Logical_Programs;
+
+public class Odd_Even_No 
+{
+public static void main(String[] args) 
+{
+	int a[]= {1,2,5,6,3,2,};
+	System.out.println("Odd Numbers : ");
+	for(int i=0; i<a.length;i++)
+	{
+		if (a[i]%2!=0)
+		{
+		System.out.println(a[i]);	
+		}
+	}
+	System.out.println("Even Numbers : ");
+	for(int i=0; i<a.length;i++)
+	{
+		if(a[i]%2==0)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
+}
