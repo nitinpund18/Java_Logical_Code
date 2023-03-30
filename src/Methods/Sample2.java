364cd1d6500adf1663d0b1b084bcea1e70b5e5ff
Shree <Shree@DESKTOP-5GK796K>
2023-03-30 07:03:26 -0700

    methods

--- src/Methods/Sample2.java
+++ src/Methods/Sample2.java
@@ -1,5 +1,12 @@
 package Methods;
 
-public class Sample2 {
-
+public class Sample2 
+{
+	public static void subtraction()
+	{
+		int i=20;
+		int j=10;
+		int sub=j-i;
+		System.out.println(sub);
+	}
 }
