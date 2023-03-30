482edab58edb31da39ec0b4daedd853750899129
Shree <Shree@DESKTOP-5GK796K>
2023-03-30 06:59:53 -0700

    methods

--- src/Methods/Sample1.java
+++ src/Methods/Sample1.java
@@ -1,5 +1,19 @@
 package Methods;
-
-public class Sample1 {
-
+import Methods2.Sample3;
+public class Sample1
+{
+	public static void addition()
+	{
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+	public static void main(String[] args) {
+		addition();				//same class--> methodName
+		Sample2.subtraction();	// different class & same package--> className.methodName
+		Sample3.multiplication();	//different package--> import package and then className.methodName
+		Sample3.multiplication();
+		addition();	
+	}
 }
