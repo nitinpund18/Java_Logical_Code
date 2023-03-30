57944c04c051d2d1d01ed7bb8f21b61a7151515f
Shree <Shree@DESKTOP-5GK796K>
2023-03-30 06:56:36 -0700

    methods

--- src/Methods/SameClass.java
+++ src/Methods/SameClass.java
@@ -1,5 +1,30 @@
 package Methods;
-
-public class SameClass {
-
+import Methods2.DiffPackageClass;
+public class SameClass
+{
+	public static void sameClassStaticMethod()
+	{
+		System.out.println("Different static method from same class");
+	}
+	public void sameClassNonStaticMethod()
+	{
+		System.out.println("Non static method from same class");
+	}
+	public static void main(String[] args) {
+		System.out.println("Program start");
+		sameClassStaticMethod();
+		DiffClass.diffClassMethod();
+		DiffPackageClass.diffPackStaticMethod();
+		
+		SameClass object= new SameClass();
+		object.sameClassNonStaticMethod();
+		
+		DiffClass object2 = new DiffClass();
+		object2.diffClassNonStaticMethod();
+		
+		DiffClass object3 = new DiffClass();
+		object3.diffClassNonStaticMethod();
+		
+		System.out.println("Program end");
+	}
 }
