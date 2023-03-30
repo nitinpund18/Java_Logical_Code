07baef9a87f22e406512e3b2f20538dc8c99c1fe
Shree <Shree@DESKTOP-5GK796K>
2023-03-30 01:15:55 -0700

    Datatype

---
+++ src/Datatype/Sample1.java
@@ -0,0 +1,25 @@
+package Datatype;
+
+public class Sample1 
+{
+	public static void main(String[] args) {
+		byte a= 127;
+		short b= 32767;
+		int c=2147483647;
+		int pincode=411028;
+		long d= 9876543210L;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		int sum=a+b;
+		long sum2=c+d;
+		System.out.println(sum);
+		System.out.println(sum2);
+		
+		float f=10.987654321987654321f;
+		double g=10.987654321987654321d;
+		System.out.println(f);
+		System.out.println(g);
+	}
+}
