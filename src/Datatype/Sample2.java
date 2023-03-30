49339b55b8945be58753111b91b85f87062bea79
Shree <Shree@DESKTOP-5GK796K>
2023-03-30 01:20:35 -0700

    Datatype2

---
+++ src/Datatype/Sample2.java
@@ -0,0 +1,18 @@
+package Datatype;
+
+public class Sample2 
+{
+public static void main(String[] args) 
+{
+	boolean a=true;
+	boolean b=false;
+	char c='C';
+	System.out.println(c);
+	int d=c;
+	System.out.println(d);
+	int e=3;
+	int sum=e+c;
+	System.out.println(sum);
+	String s="Nitin Pund";
+}
+}
