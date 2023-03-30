cc9cc43a38d016a2d62122243c3059e38f74d5e6
Shree <Shree@DESKTOP-5GK796K>
2023-03-30 07:07:35 -0700

    methods2

---
+++ src/Methods2/DiffPackageClass.java
@@ -0,0 +1,13 @@
+package Methods2;
+
+public class DiffPackageClass 
+{
+	public static void diffPackStaticMethod()
+	{
+		System.out.println("Diff class-diff package static method");
+	}
+	public void diffPackNonStaticMethod()
+	{
+		System.out.println("Diff class-diff package Non static method");
+	}
+}
---
+++ src/Methods2/Sample3.java
@@ -0,0 +1,12 @@
+package Methods2;
+
+public class Sample3 
+{
+	public static void multiplication()
+	{
+		int p=10;
+		int q=20;
+		int multi=p*q;
+		System.out.println(multi);
+	}
+}
