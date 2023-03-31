ae074e678a4bf2b7f17d509eb331c7c5095f189e
Shree <Shree@DESKTOP-5GK796K>
2023-03-31 03:39:41 -0700

    Operators

---
+++ src/Operators/Sample.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class Sample 
+{
+	public static void main(String[] args) {
+		int A=8;
+		int B=3;
+		int C=5;
+		System.out.println(A+B); //11
+		System.out.println(A-B); //5
+		System.out.println(A*B); //24
+		System.out.println(A/B); //2
+		System.out.println(A%B); //2-reminder
+	}
+}
---
+++ src/Operators/Sample2.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class Sample2
+{
+	public static void main(String[] args) {
+		int A=8, B=3, C=5;
+		
+		A+=B;
+		System.out.println(A);	//a=a+b
+		
+	}
+}
